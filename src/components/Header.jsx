c6ef0cda7c8d0ddf5cb019fb4695c97c8ec87ff0
Torcoroma <torcoromaarias@gmail.com>
2021-12-17 00:28:50 -0500

    Primer commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <h1>
+          <span>NEW</span>
+          <span>MUCSI</span>
+        </h1>
+      </div>
+    </>
+  );
+};
+export { Header };
---
+++ src/components/List.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <>
+      <div className="lista">
+        <ul></ul>
+      </div>
+    </>
+  );
+};
+export { List };
