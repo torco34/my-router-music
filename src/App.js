df6ecb1facb9583b020741e20ed03b91bc8c2e7d
Torcoroma <torcoromaarias@gmail.com>
2021-12-20 21:22:14 -0500

    AGREGADO: router version 6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-// import { BrowserRouter, Switch, Route } from "react-router-dom";
+// import { Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "./peges/Home";
+import { Reviews } from "./peges/ Reviews";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
