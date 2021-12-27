a83d4b53e05eb15afd2fb64b5fc3bc8f666f9375
Torcoroma <torcoromaarias@gmail.com>
2021-12-27 10:03:44 -0500

    Ensayo git

--- src/peges/ Reviews.jsx
+++ src/peges/ Reviews.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Reviews = () => {
   return (
     <div>
-      <h1>HOLA SOY LA PAGUINA Reviews </h1>
+      <h1>HOLA SOY LA PAGUINA Reviews este es mi mpajdjf</h1>
     </div>
   );
 };
